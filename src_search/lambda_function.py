27864a217c831051c5c4b5e6095a23cd3fc8ce73
Yuxuan-Lin <yl5445@nyu.edu>
2023-08-03 17:16:02 -0400

    Update lambda_function.py

--- src_search/lambda_function.py
+++ src_search/lambda_function.py
@@ -22,88 +22,114 @@ s3 = boto3.resource('s3')
 
 
 def lambda_handler(event, context):
-    # TODO implement
-    # return {
-    #     'statusCode': 200,
-    #     'body': json.dumps(event['queryStringParameters']['q'])
-    # }
-    # Get query text from event
-    query_text = event['queryStringParameters']['q']
-    # query_text = 'Show me photos with dogs and apples in them'
     
-    # Call lex with query text
-    lex_response = lex.recognize_text(
-            botId='EROCIKJPBZ',
-            botAliasId='TSTALIASID',
-            localeId='en_US',
-            sessionId=str(uuid.uuid4()),
-            text=query_text
-        )
-    
-    if lex_response['ResponseMetadata']['HTTPStatusCode'] != 200:
-        print("Lex returned error response")
-        return {
-            'statusCode': 200,
-            'body': json.dumps(lex_response)
-        }
-    
-    # Get search labels from lex response
-    search_labels = lex_response['sessionState']['intent']['slots']['Labels']['value']['interpretedValue'].split()
-    
-    print(search_labels)
-    
-    # Note that certain fields are boosted (^).
-    query = {
-              "size": 10,
-              "query": {
-                "fuzzy": {
-                  "labels": {
-                    "value": "",
-                    "fuzziness": "AUTO"
-                  }
-                }
-              }
+    try:
+        # Get query text from event
+        query_text = event['queryStringParameters']['q']
+        # query_text = 'Show me photos with dogs and apples in them'
+        
+        # Call lex with query text
+        lex_response = lex.recognize_text(
+                botId='EROCIKJPBZ',
+                botAliasId='TSTALIASID',
+                localeId='en_US',
+                sessionId=str(uuid.uuid4()),
+                text=query_text
+            )
+        
+        if lex_response['ResponseMetadata']['HTTPStatusCode'] != 200:
+            print("Lex returned error response")
+            return {
+                'statusCode': 200,
+                'body': json.dumps(lex_response)
             }
+        
+        # Get search labels from lex response
+        search_labels = lex_response['sessionState']['intent']['slots']['Labels']['value']['interpretedValue'].split()
+        
+        print(search_labels)
+        
+        # Note that certain fields are boosted (^).
+        query = {
+                "size": 10,
+                "query": {
+                    "fuzzy": {
+                    "labels": {
+                        "value": "",
+                        "fuzziness": "AUTO"
+                    }
+                    }
+                }
+                }
 
 
-    # Make the signed HTTP request
-    imgs = []
-    matched_obj = {}
-    
-    for search_label in search_labels:
-        query["query"]["fuzzy"]["labels"]["value"] = search_label
-        os_res = requests.get(url, auth=awsauth, headers=headers, data=json.dumps(query))
-    
-        print(os_res.text)
-        print(type(os_res.text))
+        # Make the signed HTTP request
+        imgs = []
+        matched_obj = {}
         
-        text_obj = json.loads(os_res.text)
+        for search_label in search_labels:
+            query["query"]["fuzzy"]["labels"]["value"] = search_label
+            os_res = requests.get(url, auth=awsauth, headers=headers, data=json.dumps(query))
         
-        if len(text_obj['hits']['hits']) == 0:
-            continue
-        
-        search_results = text_obj['hits']['hits']
-        
-        for sr in search_results:
-            obj_key = sr['_source']['objectKey']
-            if obj_key in matched_obj:
-                continue
-            else:
-                matched_obj[obj_key] = 0
-            s3_bucket = s3.Bucket(sr['_source']['bucket'])
-            s3_obj = s3_bucket.Object(key=sr['_source']['objectKey'])
-            s3_res = s3_obj.get()
-            s3_img = s3_res[u'Body'].read()
-            if not s3_img.startswith(b'data'):
-                my_obj = [base64.b64encode(s3_img)]
-            else:
-                my_obj = [s3_img]
-            my_json = str(my_obj[0])
-            my_json = my_json.replace("b'", "")
-            encoded_img = my_json.replace("'", "")
-            imgs.append(encoded_img)
+            print(os_res.text)
+            print(type(os_res.text))
             
-    if len(imgs) == 0:
+            text_obj = json.loads(os_res.text)
+            
+            if len(text_obj['hits']['hits']) == 0:
+                continue
+            
+            search_results = text_obj['hits']['hits']
+            
+            for sr in search_results:
+                obj_key = sr['_source']['objectKey']
+                if obj_key in matched_obj:
+                    continue
+                else:
+                    matched_obj[obj_key] = 0
+                s3_bucket = s3.Bucket(sr['_source']['bucket'])
+                s3_obj = s3_bucket.Object(key=sr['_source']['objectKey'])
+                s3_res = s3_obj.get()
+                s3_img = s3_res[u'Body'].read()
+                if not s3_img.startswith(b'data'):
+                    my_obj = [base64.b64encode(s3_img)]
+                else:
+                    my_obj = [s3_img]
+                my_json = str(my_obj[0])
+                my_json = my_json.replace("b'", "")
+                encoded_img = my_json.replace("'", "")
+                imgs.append(encoded_img)
+                
+        if len(imgs) == 0:
+            return {
+                "statusCode": 500,
+                'headers': {
+                    'Access-Control-Allow-Headers': 'Content-Type',
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+                },
+                'body':json.dumps({
+                    "message": "Could not find any photo matching the search indices"
+                })
+            }
+        
+        lambda_response = {
+            'statusCode': 200,
+            'headers': {
+                'Access-Control-Allow-Headers': 'Content-Type',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+            },
+            'body':json.dumps({
+                'message': 'Found {} photos matching the search indices'.format(len(imgs)),
+                'encoded_image': imgs
+            })
+        }
+        
+        print(lambda_response)
+        
+        return lambda_response
+    except Exception as error:
         return {
             "statusCode": 500,
             'headers': {
@@ -112,23 +138,6 @@ def lambda_handler(event, context):
                 'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
             },
             'body':json.dumps({
-                "message": "Could not find any photo matching the search indices"
+                "message": str(error)
             })
         }
-    
-    lambda_response = {
-        'statusCode': 200,
-        'headers': {
-            'Access-Control-Allow-Headers': 'Content-Type',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-        },
-        'body':json.dumps({
-            'message': 'Found {} photos matching the search indices'.format(len(imgs)),
-            'encoded_image': imgs
-        })
-    }
-    
-    print(lambda_response)
-    
-    return lambda_response
